88f59441f4b7b540fbc7fea7773f32c4374130fe
dlech <gismho@gmail.com>
2012-03-07 22:40:55 -0600

    add license and readme files

--- KeePassPluginTestUtil/Properties/AssemblyInfo.cs
+++ KeePassPluginTestUtil/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KeePassPluginTestUtil")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helper library for unit testing KeePass 2.x plugins")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("dlech")]
 [assembly: AssemblyProduct("KeePassPluginTestUtil")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
